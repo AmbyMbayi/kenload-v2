07b5271ccd51026f683edec8f9b0a9d7f54f369e
Amby Mbayi <am@workpay.co.ke>
2021-06-30 16:16:41 +0300

    navigation done

--- src/components/horizontal-menu.js
+++ src/components/horizontal-menu.js
@@ -6,40 +6,146 @@ export const menuItems = [
 		link: '/'
 	},
 	{
-		id: 11,
+		id: 2,
+		label: 'menuitems.weigh.text',
+		icon: 'uil-home-alt',
+		link: '/',
+		subItems: [
+			{
+				id: 31,
+				label: 'menuitems.weigh.list.multideck',
+				link: '/'
+			},
+			{
+				id: 202,
+				label: 'menuitems.weigh.list.lswim',
+				link: '/'
+			},
+			{
+				id: 203,
+				label: 'menuitems.weigh.list.hswim',
+				link: '/'
+			},
+			{
+				id: 204,
+				label: 'menuitems.weigh.list.tags',
+				link: '/'
+			},
+			{
+				id: 205,
+				label: 'menuitems.weigh.list.yardlist',
+				link: '/'
+			}
+		]
+	},
+
+	{
+		id: 4,
+		label: 'menuitems.prosecution.text',
+		icon: 'uil-home-alt',
+		link: '/'
+	},
+	{
+		id: 5,
+		label: 'menuitems.census.text',
+		icon: 'uil-home-alt',
+		link: '/'
+	},
+	{
+		id: 6,
+		label: 'menuitems.vehicleinspection.text',
+		icon: 'uil-home-alt',
+		link: '/',
+		subItems: [
+			{
+				id: 20,
+				label: 'menuitems.vehicleinspection.list.wideloads',
+				link: '/'
+			},
+			{
+				id: 21,
+				label: 'menuitems.vehicleinspection.list.yardvehicles',
+				link: '/'
+			}
+		]
+	},
+
+	{
+		id: 7,
+		label: 'menuitems.data.text',
+		icon: 'uil-home-alt',
+		link: '/',
+		subItems: [
+			{
+				id: 3,
+				label: 'menuitems.data.list.roadauthority',
+				link: '/'
+			}
+		]
+	},
+
+	{
+		id: 8,
 		label: 'menuitems.userslist.text',
 		icon: 'uil-users-alt',
 		link: '/users-list'
 	},
 	{
-		id: 111,
+		id: 9,
 		label: 'menuitems.specialrelease.text',
 		icon: 'uil-taxi',
 		link: '/special-release'
-	}
-	/*
+	},
 	{
-		id: 2,
-		label: 'menuitems.uielements.text',
-		icon: 'uil-flask',
+		id: 10,
+		label: 'menuitems.reporting.text',
+		icon: 'uil-taxi',
+		link: '/'
+	},
+	{
+		id: 11,
+		label: 'menuitems.settings.text',
+		icon: 'uil-home-alt',
+		link: '/',
 		subItems: [
 			{
-				id: 3,
-				label: 'menuitems.uielements.list.alerts',
-				link: '/ui/alerts'
+				id: 12,
+				label: 'menuitems.settings.list.camerasetting',
+				link: '/'
 			},
-
 			{
-				id: 8,
-				label: 'menuitems.uielements.list.grid',
-				link: '/ui/grid'
+				id: 13,
+				label: 'menuitems.settings.list.prosecutionsetting',
+				link: '/'
 			},
-
 			{
-				id: 18,
-				label: 'menuitems.uielements.list.video',
-				link: '/ui/video'
+				id: 14,
+				label: 'menuitems.settings.list.permitsetting',
+				link: '/'
+			},
+			{
+				id: 15,
+				label: 'menuitems.settings.list.trafficcontrolsetting',
+				link: '/'
 			}
 		]
-	}*/
+	},
+	{
+		id: 12,
+		label: 'menuitems.technical.text',
+		icon: 'uil-home-alt',
+		link: '/',
+		subItems: [
+			{
+				id: 20,
+				label: 'menuitems.technical.list.healthcheck',
+				link: '/'
+			},
+			{
+				id: 21,
+				label: 'menuitems.technical.list.calibrationsetting',
+				link: '/'
+			}
+		]
+	}
 ];
